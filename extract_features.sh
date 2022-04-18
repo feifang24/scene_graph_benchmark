3f17ced7d68072cab0dc40651dd7393fccef26ea
Fei Fang <feifang24@gmail.com>
2022-04-18 04:27:06 +0000

    bug fix

--- extract_features.sh
+++ extract_features.sh
@@ -1,7 +1,10 @@
 export VINVL_DATA_DIR=/home/ubuntu/s3-drive/data/vinvl_data
+
+# python tools/mini_tsv/tsv_demo.py
+
 python tools/test_sg_net.py \
   --config-file sgg_configs/vgattr/vinvl_x152c4.yaml \
-  TEST.IMS_PER_BATCH 32 \
+  TEST.IMS_PER_BATCH 1 \
   MODEL.WEIGHT pretrained_model/vinvl_vg_x152c4.pth \
   MODEL.ROI_HEADS.NMS_FILTER 1 \
   MODEL.ROI_HEADS.SCORE_THRESH 0.2 \
@@ -10,6 +13,7 @@ python tools/test_sg_net.py \
   DATA_DIR $VINVL_DATA_DIR \
   TEST.IGNORE_BOX_REGRESSION True \
   MODEL.ATTRIBUTE_ON True \
-  DATASETS.TEST "('train.yaml', 'val.yaml', 'test.yaml')"
+  DATASETS.TEST "('train.yaml', 'val.yaml', 'test.yaml')" \
+  DATASETS.FACTORY_TEST "('VGTSVDataset', 'VGTSVDataset', 'VGTSVDataset')"
 
 unset VINVL_DATA_DIR
