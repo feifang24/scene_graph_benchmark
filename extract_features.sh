cb326cbce380f6dd1d965b65523cf6e54e3ade7b
Fei Fang <feifang24@gmail.com>
2022-04-18 04:52:48 +0000

    complete bash script from end to end

--- extract_features.sh
+++ extract_features.sh
@@ -1,6 +1,7 @@
-export VINVL_DATA_DIR=/home/ubuntu/s3-drive/data/vinvl_data
+export DATA_ROOT_DIR=/home/ubuntu/s3-drive/data/
+export VINVL_DATA_DIR=vinvl_data
 
-# python tools/mini_tsv/tsv_demo.py
+python tools/mini_tsv/tsv_demo.py $VINVL_DATA_DIR
 
 python tools/test_sg_net.py \
   --config-file sgg_configs/vgattr/vinvl_x152c4.yaml \
@@ -9,11 +10,14 @@ python tools/test_sg_net.py \
   MODEL.ROI_HEADS.NMS_FILTER 1 \
   MODEL.ROI_HEADS.SCORE_THRESH 0.2 \
   TEST.OUTPUT_FEATURE True \
-  OUTPUT_DIR $VINVL_DATA_DIR \
-  DATA_DIR $VINVL_DATA_DIR \
+  OUTPUT_DIR $DATA_ROOT_DIR/$VINVL_DATA_DIR \
+  DATA_DIR $DATA_ROOT_DIR/$VINVL_DATA_DIR \
   TEST.IGNORE_BOX_REGRESSION True \
   MODEL.ATTRIBUTE_ON True \
   DATASETS.TEST "('train.yaml', 'val.yaml', 'test.yaml')" \
   DATASETS.FACTORY_TEST "('VGTSVDataset', 'VGTSVDataset', 'VGTSVDataset')"
 
+python postprocess_features.py $VINVL_DATA_DIR
+
+unset DATA_ROOT_DIR
 unset VINVL_DATA_DIR
