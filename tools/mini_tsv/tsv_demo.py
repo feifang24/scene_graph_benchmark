bf095cf3915a4ca555d9ad6fdb0142a8647b46ae
Fei Fang <feifang24@gmail.com>
2022-04-18 00:19:23 +0000

    modify tsv_demo.py to work on concadia

--- tools/mini_tsv/tsv_demo.py
+++ tools/mini_tsv/tsv_demo.py
@@ -5,6 +5,7 @@ import json
 import cv2
 import base64
 import yaml
+from tqdm import tqdm
 
 from maskrcnn_benchmark.structures.tsv_file_ops import tsv_reader, tsv_writer
 from maskrcnn_benchmark.structures.tsv_file_ops import generate_linelist_file
@@ -12,30 +13,36 @@ from maskrcnn_benchmark.structures.tsv_file_ops import generate_hw_file
 from maskrcnn_benchmark.structures.tsv_file import TSVFile
 from maskrcnn_benchmark.data.datasets.utils.image_ops import img_from_base64
 
-data_path = "tools/mini_tsv/images/"
-img_list = os.listdir(data_path)
+DATA_DIR = '/home/ubuntu/s3-drive/data'
+data_path = op.join(DATA_DIR, 'wiki_split.json')
+img_dir = op.join(DATA_DIR, 'wikicommons', 'resized')
+output_dir = op.join(DATA_DIR, 'vinvl_data')
 
-split = 'train'
+with open(data_path, 'r') as j:
+    data = json.load(j)['images']
 
-# Create .yaml file for connecting .tsv files
-yaml_dict = {attr: f'{split}.{attr}.tsv' for attr in ['img', 'hw']} # ['img', 'hw', 'label', 'linelist]
-with open(op.join(data_path, f'{split}.yaml'), 'w') as file:
-        yaml.dump(yaml_dict, file, default_flow_style=False)
+splits = ['train', 'val', 'test']
 
-# To generate a tsv file:
-img_file = os.path.join(data_path, yaml_dict['img'])
-hw_file = os.path.join(data_path, yaml_dict['hw'])
-# label_file = "tools/mini_tsv/data/train.label.tsv"
-# linelist_file = "tools/mini_tsv/data/train.linelist.tsv"
-
-rows = []
+rows_img = {split: [] for split in splits}
+rows_hw = {split: [] for split in splits}
 # rows_label = []
-rows_hw = []
-for img_p in img_list:
+
+for example in tqdm(data):
+    example_split = example['split']
+
+    img_p = example['filename']
     img_key = img_p.split('.')[0]
-    img_path = op.join(data_path, img_p)
+    img_path = op.join(img_dir, img_p)
     img = cv2.imread(img_path)
     img_encoded_str = base64.b64encode(cv2.imencode('.jpg', img)[1])
+
+    row = [img_key, img_encoded_str]
+    rows_img[example_split].append(row)
+
+    height = img.shape[0]
+    width = img.shape[1]
+    row_hw = [img_key, json.dumps([{"height":height, "width":width}])]
+    rows_hw[example_split].append(row_hw)
 
     # # Here is just a toy example of labels.
     # # The real labels can be generated from the annotation files
@@ -46,23 +53,25 @@ for img_p in img_list:
     # labels.append({"rect": [1, 1, 30, 40], "class": "Dog"})
     # labels.append({"rect": [2, 3, 100, 100], "class": "Cat"})
 
-    # row = [img_key, img_encoded_str]
-    # rows.append(row)
-
     # row_label = [img_key, json.dumps(labels)]
     # rows_label.append(row_label)
 
-    height = img.shape[0]
-    width = img.shape[1]
-    row_hw = [img_key, json.dumps([{"height":height, "width":width}])]
-    rows_hw.append(row_hw)
+# Create .yaml file for connecting .tsv files
+yaml_dicts = {split: {attr: f'{split}.{attr}.tsv' for attr in ['img', 'hw']} # ['img', 'hw', 'label', 'linelist]
+                for split in splits}
 
-tsv_writer(rows, img_file)
-tsv_writer(rows_hw, hw_file)
-# tsv_writer(rows_label, label_file)
-
-# generate linelist file
-# generate_linelist_file(label_file, save_file=linelist_file)
+for split in splits:
+    img_file = op.join(output_dir, yaml_dicts[split]['img'])
+    hw_file = op.join(output_dir, yaml_dicts[split]['hw'])
+    tsv_writer(rows_img[split], img_file)
+    tsv_writer(rows_hw[split], hw_file)
+    # label_file = "tools/mini_tsv/data/train.label.tsv"
+    # linelist_file = "tools/mini_tsv/data/train.linelist.tsv"
+    # tsv_writer(rows_label, label_file)
+    # generate linelist file
+    # generate_linelist_file(label_file, save_file=linelist_file)
+    with open(op.join(output_dir, f'{split}.yaml'), 'w') as file:
+            yaml.dump(yaml_dicts[split], file, default_flow_style=False)
 
 
 # # To access a tsv file:
