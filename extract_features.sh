9c295fac70b03a7ec6c12045909edc94e289b368
Fei Fang <feifang24@gmail.com>
2022-04-19 00:01:27 +0000

    decrease bsz; skip preprocessing since it's been run

--- extract_features.sh
+++ extract_features.sh
@@ -1,11 +1,11 @@
 export DATA_ROOT_DIR=/home/ubuntu/s3-drive/data
 export VINVL_DATA_DIR=vinvl_data
 
-python tools/mini_tsv/tsv_demo.py $VINVL_DATA_DIR
+# python tools/mini_tsv/tsv_demo.py $VINVL_DATA_DIR
 
 python tools/test_sg_net.py \
   --config-file sgg_configs/vgattr/vinvl_x152c4.yaml \
-  TEST.IMS_PER_BATCH 4 \
+  TEST.IMS_PER_BATCH 1 \
   MODEL.WEIGHT pretrained_model/vinvl_vg_x152c4.pth \
   MODEL.ROI_HEADS.NMS_FILTER 1 \
   MODEL.ROI_HEADS.SCORE_THRESH 0.2 \
